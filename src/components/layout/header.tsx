142b2420bffb32ad75b22db5ade7eb20996dd149
Shoko Okamoto <mffnoissm23@gmail.com>
2024-04-04 11:46:01 +0900

    ♻️ aタグに変更（<Link>のhref型エラー解決困難）

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
   const pathname = usePathname();
@@ -30,22 +30,23 @@ export const Header = () => {
   ];
 
   return (
-    <header className="h-screen basis-1/4 flex-shrink-0 pt-9 space-y-8 border-r border-r-indigo-300 p-4">
+    <header className="h-screen shrink-0 basis-1/4 space-y-8 border-r border-r-indigo-300 p-4 pt-9">
       <Link href="/" className="text-4xl font-black">
-        Fun Avatar<br/>Generator
+        Fun Avatar
+        <br />
+        Generator
       </Link>
       <nav>
         <ul className="space-y-2">
           {navs &&
             navs.map((nav) => {
               const isCurrent = nav.path === pathname ? true : false;
-
               return (
                 <li key={nav.id}>
-                  <Link href={nav.path} className="flex space-x-2 align-middle transition-opacity hover:opacity-80">
+                  <a href={nav.path} className="flex space-x-2 align-middle transition-opacity hover:opacity-80">
                     <Image src={nav.image} width={30} height={30} alt="icon" className="rounded-lg" />
                     <span className={isCurrent ? "font-bold text-lime-500" : "font-bold"}>{nav.label}</span>
-                  </Link>
+                  </a>
                 </li>
               );
             })}
